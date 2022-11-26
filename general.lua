2a887333828c0a1bf03f7fe21e7fa11e4c764c83
ianie <2817181586@qq.com>
2022-11-26 22:00:17 +0800

    transparent gui

--- general.lua
+++ general.lua
@@ -1,2 +1,7 @@
 vim.wo.relativenumber = true
-vim.opt.guifont = { "SauceCodePro Nerd Font Mono", ":h12" }
+
+if vim.fn.exists "g:neovide" then
+  vim.opt.guifont = { "SauceCodePro Nerd Font Mono", ":h12" }
+  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_cursor_vfx_mode = "ripple"
+end
--- plugins.lua
+++ plugins.lua
@@ -41,7 +41,7 @@ lvim.plugins = {
     "simrat39/symbols-outline.nvim",
     config = function()
       require("symbols-outline").setup({
-        autofold_depth = 1,
+        autofold_depth = 0,
 
         symbols = {
           File = { hl = "@text.uri" },
@@ -74,4 +74,22 @@ lvim.plugins = {
       })
     end
   },
+  { "xiyaowong/nvim-transparent",
+    config = function()
+      require("transparent").setup({
+        enable = false, -- boolean: enable transparent
+        extra_groups = { -- table/string: additional groups that should be cleared
+          -- In particular, when you set it to 'all', that means all available groups
+
+          -- example of akinsho/nvim-bufferline.lua
+          "BufferLineTabClose",
+          "BufferlineBufferSelected",
+          "BufferLineFill",
+          "BufferLineBackground",
+          "BufferLineSeparator",
+          "BufferLineIndicatorSelected",
+        },
+        exclude = {}, -- table: groups you don't want to clear
+      })
+    end }
 }
