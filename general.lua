eaf2433d18f65784e614cac897d540f823c031cd
ianie <2817181586@qq.com>
2022-11-28 17:19:00 +0800

    tab onedark

--- general.lua
+++ general.lua
@@ -1,7 +1,10 @@
 vim.wo.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 if vim.fn.exists "g:neovide" then
-  vim.opt.guifont = { "SauceCodePro Nerd Font Mono", ":h12" }
-  vim.g.neovide_transparency = 0.95
-  vim.g.neovide_cursor_vfx_mode = "ripple"
+    vim.opt.guifont = { "SauceCodePro Nerd Font Mono", ":h12" }
+    vim.g.neovide_transparency = 0.95
+    vim.g.neovide_cursor_vfx_mode = "ripple"
 end
--- plugins.lua
+++ plugins.lua
@@ -1,95 +1,103 @@
 lvim.plugins = {
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  {
-    "michaelb/sniprun",
-    run = "bash ./install.sh"
-  },
-  {
-    "ggandor/leap.nvim",
-    config = function()
+    {
+        "folke/trouble.nvim",
+        cmd = "TroubleToggle",
+    },
+    {
+        "michaelb/sniprun",
+        run = "bash ./install.sh"
+    },
+    {
+        "ggandor/leap.nvim",
+        config = function()
 
-    end
-  },
-  {
-    "ggandor/flit.nvim",
-    config = function()
-      require("flit").setup({
-        keys = { f = 'f', F = 'F', t = 't', T = 'T' },
-        -- A string like "nv", "nvo", "o", etc.
-        labeled_modes = "v",
-        multiline = true,
-        -- Like `leap`s similar argument (call-specific overrides).
-        -- E.g.: opts = { equivalence_classes = {} }
-        opts = {}
-      })
+        end
+    },
+    {
+        "ggandor/flit.nvim",
+        config = function()
+            require("flit").setup({
+                keys = { f = 'f', F = 'F', t = 't', T = 'T' },
+                -- A string like "nv", "nvo", "o", etc.
+                labeled_modes = "v",
+                multiline = true,
+                -- Like `leap`s similar argument (call-specific overrides).
+                -- E.g.: opts = { equivalence_classes = {} }
+                opts = {}
+            })
 
-    end
-  },
-  {
-    "kylechui/nvim-surround",
-    tag = "*",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end
-  },
-  {
-    "simrat39/symbols-outline.nvim",
-    config = function()
-      require("symbols-outline").setup({
-        autofold_depth = 0,
+        end
+    },
+    {
+        "kylechui/nvim-surround",
+        tag = "*",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+    {
+        "simrat39/symbols-outline.nvim",
+        config = function()
+            require("symbols-outline").setup({
+                autofold_depth = 0,
 
-        symbols = {
-          File = { hl = "@text.uri" },
-          Module = { hl = "@namespace" },
-          Namespace = { hl = "@namespace" },
-          Package = { hl = "@namespace" },
-          Class = { hl = "@type" },
-          Method = { hl = "@method" },
-          Property = { hl = "@method" },
-          Field = { hl = "@field" },
-          Constructor = { hl = "@constructor" },
-          Enum = { hl = "@type" },
-          Interface = { hl = "@type" },
-          Function = { hl = "@function" },
-          Variable = { hl = "@constant" },
-          Constant = { hl = "@constant" },
-          String = { hl = "@string" },
-          Number = { hl = "@number" },
-          Boolean = { hl = "@boolean" },
-          Array = { hl = "@constant" },
-          Object = { hl = "@type" },
-          Key = { hl = "@type" },
-          Null = { hl = "@type" },
-          EnumMember = { hl = "@field" },
-          Struct = { hl = "@type" },
-          Event = { hl = "@type" },
-          Operator = { hl = "@operator" },
-          TypeParameter = { hl = "@parameter" },
-        },
-      })
-    end
-  },
-  { "xiyaowong/nvim-transparent",
-    config = function()
-      require("transparent").setup({
-        enable = false, -- boolean: enable transparent
-        extra_groups = { -- table/string: additional groups that should be cleared
-          -- In particular, when you set it to 'all', that means all available groups
+                symbols = {
+                    File = { hl = "@text.uri" },
+                    Module = { hl = "@namespace" },
+                    Namespace = { hl = "@namespace" },
+                    Package = { hl = "@namespace" },
+                    Class = { hl = "@type" },
+                    Method = { hl = "@method" },
+                    Property = { hl = "@method" },
+                    Field = { hl = "@field" },
+                    Constructor = { hl = "@constructor" },
+                    Enum = { hl = "@type" },
+                    Interface = { hl = "@type" },
+                    Function = { hl = "@function" },
+                    Variable = { hl = "@constant" },
+                    Constant = { hl = "@constant" },
+                    String = { hl = "@string" },
+                    Number = { hl = "@number" },
+                    Boolean = { hl = "@boolean" },
+                    Array = { hl = "@constant" },
+                    Object = { hl = "@type" },
+                    Key = { hl = "@type" },
+                    Null = { hl = "@type" },
+                    EnumMember = { hl = "@field" },
+                    Struct = { hl = "@type" },
+                    Event = { hl = "@type" },
+                    Operator = { hl = "@operator" },
+                    TypeParameter = { hl = "@parameter" },
+                },
+            })
+        end
+    },
+    { "xiyaowong/nvim-transparent",
+        config = function()
+            require("transparent").setup({
+                enable = false, -- boolean: enable transparent
+                extra_groups = { -- table/string: additional groups that should be cleared
+                    -- In particular, when you set it to 'all', that means all available groups
 
-          -- example of akinsho/nvim-bufferline.lua
-          "BufferLineTabClose",
-          "BufferlineBufferSelected",
-          "BufferLineFill",
-          "BufferLineBackground",
-          "BufferLineSeparator",
-          "BufferLineIndicatorSelected",
-        },
-        exclude = {}, -- table: groups you don't want to clear
-      })
-    end }
+                    -- example of akinsho/nvim-bufferline.lua
+                    "BufferLineTabClose",
+                    "BufferlineBufferSelected",
+                    "BufferLineFill",
+                    "BufferLineBackground",
+                    "BufferLineSeparator",
+                    "BufferLineIndicatorSelected",
+                },
+                exclude = {}, -- table: groups you don't want to clear
+            })
+        end
+    },
+    { "olimorris/onedarkpro.nvim",
+        config = function()
+            require("onedarkpro").setup({
+
+            })
+        end
+    }
 }
